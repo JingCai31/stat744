49f361e02c80f3066b84658cc921ee9cd0e535db
JingCai31 <j.cai1991@gmail.com>
2018-04-10 17:18:08 -0400

    Add files via upload

---
+++ HW 2nd part/HW6-spatial-data.R
@@ -0,0 +1,16 @@
+## This data is from Police London
+## street-level crime, outcome, and stop and search information are provided
+## I selected force of Avon and Somerset Constabulary
+## numbers of crime are clustered in circle
+## keep zooming in, blue icons will appear, which represents individual crime
+## by clicking the blue icon, the crime type will pop up.
+
+library(leaflet)
+library(tidyverse)
+crime_london<-read.csv("2018-02-avon-and-somerset-street.csv")%>%
+  select(c(Longitude,Latitude,Crime.type))%>%
+  drop_na()
+leaflet(crime_london) %>%
+  addProviderTiles(providers$CartoDB.DarkMatter) %>%
+  addMarkers(lng = crime_london$Longitude, lat = crime_london$Latitude,
+             clusterOptions = markerClusterOptions(),popup=paste0(crime_london$Crime.type))
