dfd25fbcdb1f0b508c839d721dd38b2b2cd6dd0d
JingCai31 <j.cai1991@gmail.com>
2018-04-21 17:54:33 -0400

    Add files via upload

---
+++ HW 2nd part/HW5-high-dimensional-data.R
@@ -0,0 +1,18 @@
+library(gdata)
+library(colorspace)
+library(rlang)
+library(ash)
+
+# myopia or nearsightedness(colomn 3) whether have some relationship with covariates.
+# before do the clustering, we could check whether they have some patterns indeed.
+dat2 = read.xls ("myopia.xls", sheet = 1, header = TRUE)
+pal <- rainbow_hcl(length(levels(as.factor(dat2$MYOPIC))))
+col <- pal[as.numeric(dat2$MYOPIC)+1]
+dim(dat2)
+animate_xy(dat2[, 6:10], guided_tour(lda_pp(dat2$MYOPIC)), sphere = TRUE, col=col,
+           axes = "bottomleft")
+##We could tell even though the boundary is not that strict, there is still clusters between.
+###
+library(aplpack)
+a<-faces(dat2[1:20,3:18],face.type = 2)
+plot.faces(a)
